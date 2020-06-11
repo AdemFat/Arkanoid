752908cee39fdd1e6e00351db4a734dd251df184
AdemFat <medk57597@gmail.com>
2020-06-11 16:13:34 +0100

    arkanoid

---
+++ Arkanoid/Assets/Scenes/Racket.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    // Movement Speed
+    public float speed = 150;
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+    }
+    
+    void FixedUpdate () 
+    {
+        // Get Horizontal Input
+        float h = Input.GetAxisRaw("Horizontal");
+        // Set Velocity (movement direnction * speed)
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+   }
+
+}
+
+
+        
+    
+
